bee3a43e070247cd060ee5396d10be7fc3baa6b0
garciajordy <jordygarcia0119@gmail.com>
2021-06-08 16:59:01 +0300

    Update domain name

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
-        origins 'http://back-end-name-metlify'
+        origins 'http://course-tracker-api.netlify.app/'
         resource "*", headers: :any, methods: [:get, :put, :post, :patch, :delete, :options, :head], credentials: true
     end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "back-end-netlify-name.com"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "http://course-tracker-api.netlify.app/"
 else
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
