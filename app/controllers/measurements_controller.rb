8eeb9c882b89001cdae6e58231b10934228107f8
garciajordy <jordygarcia0119@gmail.com>
2021-06-12 22:42:43 +0300

    Add course and measurement endpoints

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,2 +1,34 @@
 class MeasurementsController < ApplicationController
+    include CurrentUserConcern
+    def create
+        @measurement = Measurement.create(amount: params[:measurement][:amount],
+            user_id: @current_user.id,
+            course_id: params[:measurement][:course_id])
+            if @measurement
+                render :json => {
+                    measurement: @measurement,
+                    status: :created
+                }
+            else
+                render json: { status: 401 }
+            end
+    end
+
+    def update
+        @measurement.find(params[:measurement][:id])
+        @measurement.update(amount: params[:measurement][:amount])
+        if @measurement.save
+            render :json => {
+                status: :success
+            }
+        else
+            render json: { status: 401 }
+        end
+    end
+
+    def destroy
+        @measurement.find(params[:id])
+        @measurement.destroy
+        render json: { status: :success}
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'courses/index'
-  get 'courses/show'
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
+  resources :courses, only: [:index, :show]
+  resources :measurements, only: [:create, :update, :destroy]
   root to: "static#home"
 end
